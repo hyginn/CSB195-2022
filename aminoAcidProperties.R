b2e625ce163649a711063cef620a61ef1f48b80d
hyginn <boris.steipe@utoronto.ca>
2022-09-26 21:42:35 -0400

    V 1.1 Updated and expanded

--- aminoAcidProperties.R
+++ aminoAcidProperties.R
@@ -4,14 +4,14 @@
 #              R code demonstrating amino acid properties with ChimeraX
 #              visualization
 #
-# Version:  1.0
+# Version:  1.1
 #
 # Date:     2022-09
 # Author:   Boris Steipe (boris.steipe@utoronto.ca)
 #
 # Versions:
-#
-#           1.0    First version
+#           1.1    Clean up post lecture.
+#           1.0    First version for lecture.
 #
 #
 # PREREQUISITES:
@@ -32,7 +32,25 @@
 #
 # ==============================================================================
 #
-#
+
+
+#TOC> ==========================================================================
+#TOC> 
+#TOC>   Section  Title                                               Line
+#TOC> -------------------------------------------------------------------
+#TOC>   1        ChimeraX REMOTE SCRIPTING                             55
+#TOC>   1.1        Defining a Port                                     73
+#TOC>   1.2        Prepare ChimeraX                                    95
+#TOC>   1.3        A function to communicate with ChimeraX            115
+#TOC>   2        Visualize the 7ACN fold.                             193
+#TOC>   2.1        Composition of the protein's structure             210
+#TOC>   2.1.1          Visualize a "slice" through the protein        241
+#TOC>   2.1.2          Continuous translation  via a for-loop ...     255
+#TOC>   2.2        Study a single amino acid in context:              289
+#TOC> 
+#TOC> ==========================================================================
+
+
 #
 # =    1  ChimeraX REMOTE SCRIPTING  ===========================================
 
@@ -49,12 +67,12 @@
 # we need to do is:
 #  - open ChimeraX;
 #  - tell it to listen on a specific "port";
-#  - send commands to that port via httr::
+#  - send commands to that port via httr:: package functions.
 
 
 # ==   1.1  Defining a Port  ===================================================
 
-# The httr:: package needs to be available
+# The httr:: package needs to be installed ...
 
 if (! requireNamespace("httr", quietly = TRUE)) {
   install.packages("httr")
@@ -64,35 +82,41 @@ if (! requireNamespace("httr", quietly = TRUE)) {
 #  browseVignettes("httr")    # available vignettes
 #  data(package = "httr")     # available datasets
 
-# We need to think of a port. Any available port number between 49152-65535 is
-# fine. We'll choose 61803.
+# We need to define a port - that's something like a mailbox number which
+# processes agree on to share information. Any available port number
+# between 49152-65535 is fine. We'll choose 61803.
 
 CXPORT <- 61803
 
-# Check that our current version of R supports sockets (default since V 3.3)
+# Check that our current version of R supports sockets (default since V 3.3):
 capabilities("sockets")   # MUST be TRUE. If not, don't continue.
 
 
-# ==   1.2  Open ChimeraX  =====================================================
+# ==   1.2  Prepare ChimeraX  ==================================================
 
-#  - Open a fresh, new session of recently updated version of ChimeraX
+#  - Open a fresh, new session of a recently updated version of ChimeraX
 #  - type:
-#
 #       remotecontrol rest start port 61803
 #
-#    ... or whatever the value of CXPORT is.
+#    (... or whatever value you have given CXPORT.)
 
 # Now watch what happens in ChimeraX when you execute the following line:
 ( x <- httr::GET("http://127.0.0.1:61803/run?command=open+7ACN") )
 
+# (127.0.0.1 is the "localhost" IP address. Only this one is supported for
+#  security reasons, but ChimeraX can be opened: cf.
+#  https://www.rbvi.ucsf.edu/pipermail/chimerax-users/2017-July/000113.html .)
+
 # (7ACN is a structure of Aconitase - an enzyme of the citric acid cycle. Cf.
 #   - https://pdb101.rcsb.org/motm/154
-#   - )
+#   - https://pdb101.rcsb.org/motm/89
+#   - https://en.wikipedia.org/wiki/Citric_acid_cycle )
 
-
-# To make this convenient, we need a function -
+# ==   1.3  A function to communicate with ChimeraX  ===========================
+#
+# To make it convenient to send commands to Chimera, we need a function -
 #   - that accepts ChimeraX commands as a parameter;
-#   - that sends the command to Chimera, correctly formatted
+#   - that sends the command to Chimera, correctly formatted;
 #   - that captures output which we may receive.
 #
 # Here is such a custom function:
@@ -107,8 +131,17 @@ CX <- function(cmd, port = CXPORT, quietly = FALSE) {
   #
   # Value:  the reply by ChimeraX, invisibly.
 
+  # (A) construct the base address, port, and command:
   CXREST <- sprintf("http://127.0.0.1:%s/run?", CXPORT)
 
+  # (B) sanitize the user-enterd variable cmd to be properly encoded in
+  # a http message. (No blanks, some specially handled characters, ...)
+  # Here we use gsub(), which seeks for a pattern defined in its first
+  # argument, substitutes the contents of the second argument, in the
+  # string that is identified with the third argument.
+  #
+  # Patterns are defined as "regular expressions", we'll encounter those later.
+  #
   cmd <- gsub("(^\\s+)|(\\s+$)", "", cmd)  # trim whitespace
   # percent encode reserved characters
   cmd <- gsub("%",   "%25", cmd)          #   %
@@ -129,99 +162,191 @@ CX <- function(cmd, port = CXPORT, quietly = FALSE) {
   cmd <- gsub("\\+", "%2B", cmd)          #   +
 
   cmd <- gsub("\\s+", "+", cmd)            # whitespace to "+"
-  cmd <- URLencode(cmd)                    # encode special characters
+  cmd <- URLencode(cmd)                    # encode other special characters
+
+  # Combine the base-address and the current contents of cmd ...
   cmd <- paste0(CXREST, "command=", cmd, collapse = "")
 
+  # send the command to ChimeraX, and capture the response ...
   r <- httr::GET(cmd)
+  # ... the response is a list-object and we can analyze it.
 
-  if (! r$status_code == 200) {
+  if (! r$status_code == 200) {  # If we did NOT receive a "success" status code
     stop("ChimeraX returned status code %d", r$status_code)
   }
 
   if (length(r$content) == 0) {
     reply <- ""
   } else {
-    reply <- rawToChar(r$content)
+    reply <- rawToChar(r$content)  # reformat
   }
 
   if (quietly == FALSE) {
-    cat(reply)
+    cat(reply)                     # print the response
   }
 
-  return(invisible(reply))
+  return(invisible(reply))         # return the reply  but do not also
+                                   # print it.
 
 }
 
-# Use the function to set up a visualization
+# =    2  Visualize the 7ACN fold.  ============================================
+#
+# Use the CX() function to set up a visualization of the 7ACN fold.
 
-# CX("camera sbs")
-CX("camera mono")
+CX("camera sbs")   # practice your stereo vision
+# CX("camera mono")
 CX("lighting soft")
 CX("lighting shadows true intensity 0.8")
-# define a color gradient to be able to follow the fold of the protein
+
+# Define a color gradient to be able to follow the fold of the protein
 # cf. https://www.cgl.ucsf.edu/chimerax/docs/user/commands/colornames.html
 CX("color sequential #1 & protein target abc palette #B0C4DE:rgb(44,50,56):orchid:maroon")
+
+# Study the fold. Note where it begins, how different elements of "secondary
+# structure" (helices, strands) form compact domains, and how the domains
+# assemble to a structural whole.
+
+# ==   2.1  Composition of the protein's structure  ============================
+
+# Now change the view to show the actual atoms (as little spheres.)
+#
 CX("hide cartoons")
 CX("show atoms")
 CX("style sphere")
+
+# Note the little red dots. These are water molecules (H2O); what you see
+# is the position of each oxygen atom. We hide them so we don't clutter
+# the screen too much.
 CX("hide ::name='HOH'")
 
+# First we color all atoms gray ...
 CX("color #1 & protein slate gray")
 
-# Select sidechains of hydrophobic amino acids ...
+# Then we select two categories of amino acids.
+# Select sidechains of hydrophilic amino acids ...
+# (these have atoms in their side-chains that make favourable
+# interactions with water)
 CX("select :SER, ASN, ASP, GLN, GLU, ARG, LYS, HIS & sidechain")
 CX("color sel royal blue")
 
-# ... and hydrophilic amino acids ...
+# ... and hydrophobic amino acids ...
+# (these have mostly only carbon atoms in their side chains, which gives
+#  them an "oily" nature)
 CX("select :VAL, LEU, ILE, MET, PHE, TRP & sidechain")
 CX("color sel gold")
-CX("select clear")
+CX("select clear")  # clear the selection so it doe not clutter our view
 
+
+# ===   2.1.1  Visualize a "slice" through the protein   
+#
 # set clipping planes to show a slice of the protein
 CX("clip near -2.0 far 2.0 position #1")
 
-# move forward and back
-CX("move z 0.5")
-CX("move z 0.5")
-CX("move z 0.5")
-CX("move z -0.5")
-CX("move z -0.5")
-CX("move z -0.5")
+# move the structure away from us and towards us...
+CX("move z 1.0")
+CX("move z 1.0")
+CX("move z 1.0")
+CX("move z -1.0")
+CX("move z -1.0")
+CX("move z -1.0")
 
-CX("clip near -28.0 far -24.0 position #1")
-delta <- 0.025
-N     <- 2000
-for (i in c(1:N, -(1:N))) {
+
+# ===   2.1.2  Continuous translation  via a for-loop ...
+# ... instead of manually moving along a a view step by step:
+
+CX("clip near -32.0 far -28.0 position #1")
+
+delta <- 0.06     # step-size
+N     <- 1000     # number of steps
+for (i in c(1:N, -(1:N))) { # Do this N times forward and N times back
 #  print(sprintf("move z %f", sign(i) * delta))
   CX(sprintf("move z %f", sign(i) * delta))
   CX("wait 1")
 }
 
-CX("clip near 0.0 far 20.0 position #1")
-CX("roll y 0.1")
-CX("stop")
+# Watch the protein slowly move through the slice-zone. Pay particular
+# attention to where the blue and yellow sidechains are located. Inside or
+# on the surface of the protein? Clustered, or distributed? Random, or non-
+# random?
 
-# Reset our view
+# (If this takes too long and you want to abort it, just click on the red
+# "Stopsign" in the menu bar of the Console pane.)
+
+# As an alternative to a sliding motion, we can rotate the protein slowly:
+CX("clip near 0.0 far 20.0 position #1")
+CX("roll y 0.1")   # (Documentation for all commands is available via the
+                   #  ChimeraX help functions. Easiest access is
+                   #  by clicking the hyperlinked command in the log window
+                   #  of Chimerax. It is either open, or can be opened via
+                   #  the mnu: Tools > Log.
+                   #
+CX("stop") # stop the rotation
+
+# Task: what have you observed? Why is this so? What does this imply?
+
+
+# ==   2.2  Study a single amino acid in context:  =============================
+
+# Reset our view ...
 CX("clip off")
 CX("hide atoms")
 CX("show cartoons")
 CX("color sequential #1 & protein target abc palette #B0C4DE:rgb(44,50,56):orchid:maroon")
-CX("hide cartoons")
 
-# create a second copy to work with a single residue only
+# Let's focus on a single amino acid and examine its context.
+
+# Create a second copy of the protein and delete everything but this single
+# amino acid, to work with a single residue only. (Isn't there a way to
+# duplicate just one residue or other molecule? IDK.)
 CX("open 7acn")
+
+# Open the Sequence viewer window:
 CX("sequence chain #2/A")
 
-# ... select a residue
-CX("select ~sel")
-CX("delete sel")
-CX("show atoms")
-CX("surface")
-CX("style stick")
+# To choose an amino acid of a certain type hover over it.
+# You will get information like "7acn #1 \A ARG 114 CG" - which is:
+#
+#   7acn: the PDB ID
+#   #1:   model number 1
+#   \A:   chain A
+#   ARG:  amino acid type (ARG -> arginine)
+#   114:  position in sequence  ("sequence number")
+#   CG:   atom topology, "Cgamma", third atom of the side-chain
 
-=
-CX("remotecontrol rest stop")  # release the socket
-# Done.
+# Let's display the residue with a solvent accessible surface.
+# If, say, your amino acid is HIS 298, define this as a named selection:
+CX("name myAA #2/A:298")              # define
+CX("select #2/A")                     # select the entire chain
+CX("select subtract myAA")            # subtract the named selection
+CX("delete sel")                      # delete what is selected, myAA remains
+CX("view myAA")                       # center the view
+CX("show myAA atoms")                 # show atoms, and ...
+CX("style myAA ball")                 # style as ball-and-stick
+CX("color myAA byelement target ab")  # color by element
+CX("surface myAA")                    # compute surface
+CX("color myAA #AAAADD target s")     # color wispy pale blue
+CX("transparency myAA 80 target s")   # set high transparency
+
+# Now lets calculate a surface of the protein _without_ myAA, to see how the
+# amino acid is packed against the rest of the structure.
+CX("name myAA")                       # list the selection again to remind us
+CX("select #1/A:298")                 # select ...
+CX("delete sel")                      # ... and delete it
+CX("surface #1")                      # compute a surface
+CX("color #1 #FFB7C5 target s")       # color it cherry-blossom pink
+CX("transparency #1 70 target s")     # set transparency
+
+# More things we could do is to:
+#   - view atoms that are a certain distance _around_ mAA (command "zone");
+#   - show hydrogen bonds of myAA (command "hbonds");
+#   - label the residue (command "label");
+#   - export a high-resolution image for our journal (command "save");
+#   - ... and more.
+
+
+# When we are done ...
+CX("remotecontrol rest stop")  # ... release the socket.
 
 
 

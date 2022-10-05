93e261ff97d3d2cedf5223ed1aa1eb1d2cd38b80
hyginn <boris.steipe@utoronto.ca>
2022-10-05 14:28:16 -0400

    Added: a tool to grep() keywords in aaindex; a colour palette for amino acids; a tool to read Google sheets as csv; a tool to plot amino acid vectors as styyled 2D scatterplots

--- .util.R
+++ .util.R
@@ -12,20 +12,24 @@
 
 
 #TOC> ==========================================================================
-#TOC> 
-#TOC>   Section  Title                                         Line
-#TOC> -------------------------------------------------------------
-#TOC>   1        Remote control of ChimeraX                      21
-#TOC>   2        A progress bar for long-running code            96
-#TOC> 
+#TOC>
+#TOC>   Section  Title                                           Line
+#TOC> ---------------------------------------------------------------
+#TOC>   1        Remote control of ChimeraX                        28
+#TOC>   2        A progress bar for long-running code             103
+#TOC>   3        Find Keywords in aaindex                         126
+#TOC>   4        A colour palette for amino acids                 164
+#TOC>   5        Reading Google sheets                            197
+#TOC>   6        Plotting amino acids as 2D scatterplot           244
+#TOC>
 #TOC> ==========================================================================
 
 
 # =    1  Remote control of ChimeraX  ==========================================
 CXPORT <- 61803
-cat(sprintf("The ChimeraX port (CXPORT) is now defined as %d.\n", CXPORT))
+cat(sprintf("  Defining ChimeraX port (CXPORT) as %d.\n", CXPORT))
 
-cat("Defining CX() ...")
+cat("  Defining CX() ...\n")
 CX <- function(cmd, port = CXPORT, quietly = FALSE) {
   # send a command to ChimeraX listening on port CXPORT via its REST
   # interface.
@@ -98,7 +102,7 @@ CX <- function(cmd, port = CXPORT, quietly = FALSE) {
 
 # =    2  A progress bar for long-running code  ================================
 
-cat("Defining pBar() ...")
+cat("  Defining pBar() ...\n")
 pBar <- function(i, l, nCh = 50) {
   # Draw a progress bar in the console
   # i: the current iteration
@@ -119,7 +123,181 @@ pBar <- function(i, l, nCh = 50) {
   }
 }
 
+# =    3  Find Keywords in aaindex  ============================================
+
+cat("  Defining grepAAindex() ...\n")
+
+grepAAindex <- function(key, el = "D") {
+  # Search for strings in an aaindex element
+  # key:   a regular expression pattern
+  # el:    which element to search. Default "D" (Definition), also useful might
+  #        be "T" (Titles).
+  # value: a character vector, length 0 if nothing found.
+
+  library(seqinr)
+  data("aaindex")
+
+  dat <- character(length(aaindex))
+  for (i in 1:length(aaindex)) {
+    dat[i] <- aaindex[[i]][el]
+  }
+  names(dat) <- 1:length(aaindex)
+
+  sel <- grep(key, dat)
+  return(dat[sel])
+}
+
+# grepAAindex("[Hh]ydroph")
+# idx <- 544
+# aaindex[[idx]]$D
+# aaindex[[idx]]$T
+# cat(sprintf("\n%s\t%s", names(aaindex[[idx]]$I), aaindex[[idx]]$I))
+#
+# grepAAindex("[Vv]olum")
+# idx <- 150
+# aaindex[[idx]]$D
+# aaindex[[idx]]$T
+# cat(sprintf("\n%s\t%s", names(aaindex[[idx]]$I), aaindex[[idx]]$I))
 
 
-cat("done.\n")
+
+# =    4  A colour palette for amino acids  ====================================
+
+cat("  Defining AACOLS ...\n")
+
+# A colour palette for amino acid properties
+AACOLS <- character()
+AACOLS["R"] <- "#5770ff" # Positive
+AACOLS["K"] <- "#4785EE" #
+AACOLS["H"] <- "#37a1de" #
+AACOLS["E"] <- "#ff6f59" # Negative
+AACOLS["D"] <- "#ff7391" #
+AACOLS["N"] <- "#C9D4FF" # Hydrophilic
+AACOLS["Q"] <- "#CADFFC" #
+AACOLS["S"] <- "#CBEAF9" #
+AACOLS["T"] <- "#CDF5F7" #
+AACOLS["Y"] <- "#FBFFC9" # Hydrophobic
+AACOLS["W"] <- "#EDFDC8" #
+AACOLS["F"] <- "#DFFCC8" #
+AACOLS["I"] <- "#D2FBC8" #
+AACOLS["L"] <- "#C4FAC7" #
+AACOLS["M"] <- "#B7F9C7" #
+AACOLS["V"] <- "#A9F8C7" #
+AACOLS["A"] <- "#9CF7C7" #
+AACOLS["G"] <- "#d2d2d2" # Glycine
+AACOLS["C"] <- "#fff963" # Cysteine
+AACOLS["P"] <- "#edc06d" # Proline
+AACOLS <- gsub("$", "AA", AACOLS)  # Make the colors 33% transparent
+# AACOLS <- gsub("$", "80", AACOLS)  # Make the colors 50% transparent
+# AACOLS <- gsub("$", "55", AACOLS)  # Make the colors 67% transparent
+# AACOLS <- gsub("AA$", "", AACOLS) # Remove transparency
+# barplot(rep(1, 20), col = AACOLS, names.arg = names(AACOLS), cex.names=0.5)
+
+
+# =    5  Reading Google sheets  ===============================================
+
+cat("  Defining read.gsheet() ...\n")
+
+read.gsheet <- function(URL, sheet, ...) {
+  # Read a sheet from a Google shets URL.
+  # URL: the URL of the Google sheets spreadsheet. Note: the document must
+  #      have been opened to read for everyone with the URL.
+  # sheet: the name of the sheet
+  # ... : other arguments that are passed to read.csv()
+  # value: a dataframe
+
+  # We assume the URL was received from the sheet's sharing button, thus
+  # we parse out only the ID
+
+  ID <- regmatches(URL, regexec("/d/([^/]+)/", URL))[[1]][2]
+
+  # make a retrieval URL
+  URL <- sprintf("https://docs.google.com/spreadsheets/d/%s%s%s",
+               ID,
+               "/gviz/tq?tqx=out:csv&sheet=",
+               gsub(" ", "+", sheet))
+
+  # the GET() function from httr will get the data.
+  response <- httr::GET(URL)
+  if (!httr::status_code(response) == 200) {
+    stop(sprintf("Server status code was \"%s\".",
+                 as.character(httr::status_code(response))))
+  }
+
+  x <-as.character(response)
+  x <- strsplit(x, "\n")[[1]]
+  x[1] <-      gsub("\\\"", "", x[1])
+  cNames <-  strsplit(x[1], ",")[[1]]  # Restore the original names from
+                                       # row 1 so we don't have to use the
+                                       # names that R assigns by default.
+  tbl <- utils::read.csv(text = x)
+  colnames(tbl) <- cNames
+
+  return(tbl)
+
+}
+
+ # X <- "https://docs.google.com/spreadsheets/d/1tRCPhaua5cjcH_0DuZOiv8BVbdr_V6miC2JeKiOYj-o/edit?usp=sharing"
+ # Y <- "AA styles"
+ # Z <- read.gsheet(X, Y)
+
+# =    6  Plotting amino acids as 2D scatterplot  ==============================
+
+cat("  Reading Google sheet AADAT ...\n")
+
+AADAT <- read.gsheet("https://docs.google.com/spreadsheets/d/1tRCPhaua5cjcH_0DuZOiv8BVbdr_V6miC2JeKiOYj-o/edit?usp=sharing"
+                     , "AA styles")
+
+cat("  Defining plotAA() ...\n")
+
+plotAA <- function(x, y, aaDat = AADAT, ...) {
+  # Plot amino acids in 2D
+  # x, y: numeric vectors named vectors as Aaa three letter code. They can be
+  #       passed in any order, but the order must be the same for x and y
+  # ... : other arguments that are passed to plot()
+  # aaDat: a dataframe with plotting properties.
+  #        (Default: AADAT - defined above)
+  # value: none. A plot is produced.
+
+  o <- names(x)
+  if (! all(o %in% aaDat$Aaa)) {
+    stop("Names of first vector do not match three-letter codes.")
+  }
+  if (! all(o %in% names(y))) {
+    stop("Names of first and second vector do not match.")
+  }
+  if (! all(o == names(y))) {
+    stop("Order of first and second vector is not the same.")
+  }
+  ord <- match(o, aaDat$Aaa) # index vector that maps the order of input
+                             # to the order in aaDat
+
+  # rescale aaDat$vol between 0.7 and 5
+  CEXMIN <- 0.7
+  CEXMAX <- 4.3
+  ci <- min(aaDat$vol)
+  ca <- max(aaDat$vol)
+  cexVol <- ((aaDat$vol - ci) / (ca - ci) * CEXMAX) + CEXMIN
+  names(cexVol) <- aaDat$Aaa
+
+  plot(x, y,
+       pch = aaDat$pch[ord],
+       cex = cexVol[ord],
+       col = gsub("..$", "FF", aaDat$col[ord]),
+       bg = aaDat$col[ord])
+
+  CEXMIN <- 0.7
+  CEXMAX <- 1.3
+  cexVol <- ((aaDat$vol - ci) / (ca - ci) * CEXMAX) + CEXMIN
+  names(cexVol) <- aaDat$Aaa
+  text(x, y, labels = aaDat$A[ord], cex = cexVol[ord], col="#000000AA")
+}
+
+# library(seqinr)
+# data("aaindex")
+# x <- aaindex[[150]]$I
+# y <- aaindex[[544]]$I
+# plotAA(x, y)
+
+cat("Done.\n")
 # [END]
